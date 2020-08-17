c9ed5992a63d74462cc331292ca8e4adb68bdbc4
Krakarn <linus.engelbrektsson@gmail.com>
2020-08-17 13:05:36 +0200

    Lowered target JavaScript version

--- dist/messages/message.js
+++ dist/messages/message.js
@@ -1,1 +1,6 @@
-export const createMessageHandler = (processMessageMap) => (message) => processMessageMap[message.type](message);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createMessageHandler = void 0;
+exports.createMessageHandler = function (processMessageMap) { return function (message) {
+    return processMessageMap[message.type](message);
+}; };
